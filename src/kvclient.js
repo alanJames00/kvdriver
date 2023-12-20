3fcdfcb355458afe7ab58a0d5014e980154613cf
alanjames00 <alanjames6282@gmail.com>
2023-12-21 00:20:34 +0530

    make kvclient a class

--- src/kvclient.js
+++ src/kvclient.js
@@ -1,44 +1,42 @@
 // kv client
 const axios = require('axios');
 
-// Check for email and global api key
-async function validateCreds(email, globalApiKey, accountID) {
+class KVClient {
 
-    try {
-        
-        const headers = {
-            'Content-Type':'application/json',
-            'X-Auth-Email': email,
-            'X-Auth-Key'  : globalApiKey,
+    // init the constructor
+    constructor(email, globalApiKey, AccountID) {
+        this.email = email;
+        this.globalApiKey = globalApiKey;
+        this.AccountID = AccountID;
+
+        // create the headerObj
+        this.headersObj = {
+            'Content-Type' : 'application/json',
+            'X-Auth-Email' : this.email,
+            'X-Auth-Key'   : this.globalApiKey,
         }
+    };
 
-        // make a sample request to verify the credentials
-        const resp = await axios.get('https://api.cloudflare.com/client/v4/accounts/fe71379a46f04581143516878e5da3e5/storage/kv/namespaces', { headers });
-        console.log(resp.data.success);
-        // check if success is equal to true
-        if(resp.data.success == true) {
-            console.log('Token(s) Verified Successfully');
+    // Function Definitions 
+
+    async validateCreds() {
+
+        try{
+
+            const resp = await axios.get(`https://api.cloudflare.com/client/v4/accounts/${this.AccountID}/storage/kv/namespaces`, { headers: this.headersObj });
+            console.log(resp.data.success);
+            
+            // returns the success status of validate credentials
+            return resp.data.success;
+        }
+        catch(err) {
+            console.log(err);
         }
     }
-    catch(e){
-        console.log(e);
-        console.log('Missing Or Invalid Credentials');
-    }
+
 }
 
-
-async function getPlaceholderData() {
-
-    const resp = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
-    console.log(resp.data);
-}
-
-// getPlaceholderData();
-
-module.exports = {
-    validateCreds,
-}
-
+module.exports = KVClient;
 
 
 
