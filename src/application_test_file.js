57960e9f620c4d39b65d2f7dc3b5f30d02407efe
alanjames00 <alanjames6282@gmail.com>
2023-12-21 00:03:29 +0530

    fix the headers for axios

--- src/application_test_file.js
+++ src/application_test_file.js
@@ -3,10 +3,9 @@ require('dotenv').config({path: '../.env'})
 
 // import the credentials
 
-// KVClient.validateCreds()
 
 const email = process.env.EMAIL_ID;
 const globalApiKey = process.env.GLOBAL_TOKEN;
 
-console.log(email);
-console.log(globalApiKey);
+
+KVClient.validateCreds(email, globalApiKey, '00');
--- src/kvclient.js
+++ src/kvclient.js
@@ -1,10 +1,51 @@
 // kv client
 const axios = require('axios');
 
+// Check for email and global api key
+async function validateCreds(email, globalApiKey, accountID) {
+
+    try {
+        
+        const headers = {
+            'Content-Type':'application/json',
+            'X-Auth-Email': email,
+            'X-Auth-Key'  : globalApiKey,
+        }
+
+        // make a sample request to verify the credentials
+        const resp = await axios.get('https://api.cloudflare.com/client/v4/accounts/fe71379a46f04581143516878e5da3e5/storage/kv/namespaces', { headers });
+        console.log(resp.data.success);
+        // check if success is equal to true
+        if(resp.data.success == true) {
+            console.log('Token(s) Verified Successfully');
+        }
+    }
+    catch(e){
+        console.log(e);
+        console.log('Missing Or Invalid Credentials');
+    }
+}
+
+
 async function getPlaceholderData() {
 
     const resp = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
     console.log(resp.data);
 }
 
-getPlaceholderData();
+// getPlaceholderData();
+
+module.exports = {
+    validateCreds,
+}
+
+
+
+
+
+
+
+
+
+
+// https://api.cloudflare.com/client/v4/accounts/fe71379a46f04581143516878e5da3e5/storage/kv/namespaces
