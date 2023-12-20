b26f7f57386213b2459136537d174244ae479ce4
alanjames00 <alanjames6282@gmail.com>
2023-12-20 22:45:44 +0530

    setup axios for testing

--- src/kvclient.js
+++ src/kvclient.js
@@ -1,1 +1,10 @@
 // kv client
+const axios = require('axios');
+
+async function getPlaceholderData() {
+
+    const resp = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
+    console.log(resp.data);
+}
+
+getPlaceholderData();
